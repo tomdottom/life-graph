68648a6b6291954f839866db3173f3f6cee08638
tom <thomas.marks@getmetric.com>
2014-07-21 23:45:13 +0100

    Reimpement as one page js App

---
+++ js/lifegraph.js
@@ -0,0 +1,119 @@
+function createRoutes() {
+    var router = new Grapnel();
+
+    var date = {};
+
+    router.get('birthdate/:day/:month/:year', function(req){
+        date.day = req.params.day;
+        date.month = req.params.month;
+        date.year = req.params.year;
+
+        console.log(date);
+    });
+
+    if(_.all(date)){
+        return new Date(date.year, (date.month - 1), date.day);
+    }
+}
+
+function drawLifeGraph(birthDate) {
+    var averageAgeInDays = 29219,
+        today = Date.today(),
+        endDate = birthDate.clone().addDays(averageAgeInDays),
+        birthMonth = birthDate.getMonth(),
+        birthYear = birthDate.getYear(),
+        endMonth = endDate.getMonth(),
+        endYear = endDate.getYear(),
+        daysLived = birthDate.getDaysBetween(today);
+
+    var rows = endDate.getYear() - birthDate.getYear();
+
+    var lifeDays = _.template([
+        
+    ]);
+    var lifeClock = _.template([
+        '<div class="month-clock-wrapper">',
+            '<div class="month-clock">',
+            '<div class="date">',
+            '<% _.forEach(days, function(day) {%>',
+                '<div><%- day %></div>',
+            '<% }); %>',
+            '</div>',
+            '<div class="message">days lived to the full</div>',
+            '</div>',
+        '</div>',
+    ].join('\n'));
+
+    var headerRow = _.template([
+        '<tr class="header-row">',
+            '<td class="year-col"></td>',
+            '<td class="data-col">Jan</td>',
+            '<td class="data-col">Feb</td>',
+            '<td class="data-col">Mar</td>',
+            '<td class="data-col">Apr</td>',
+            '<td class="data-col">May</td>',
+            '<td class="data-col">June</td>',
+            '<td class="data-col">July</td>',
+            '<td class="data-col">Aug</td>',
+            '<td class="data-col">Sep</td>',
+            '<td class="data-col">Oct</td>',
+            '<td class="data-col">Nov</td>',
+            '<td class="data-col">Dec</td>',
+            '<td class="age-col">Age</td>',
+        '</tr>'
+    ].join('\n'));
+
+    var dateRow =  _.template([
+        '<tr class="data-row">',
+            '<td class="year-col"><%- year %></td>',
+            '<% _.forEach (months, function(m) {',
+                'if (m.isBefore(today)) { %>',
+                    '<td class="data-col month-lived"></td>',
+                    '<% } else { %>',
+                    '<td class="data-col month-to-live"></td>',
+                    '<% }',
+            '}) %>',
+            '<td class="age-col"><%- yearCnt %></td>',
+        '</tr>',
+    ].join('\n'));
+
+    var content = [];
+
+    content.push(lifeClock({
+        days: _.map(('' + daysLived).split(''), function(d) {return +d;})
+    }));
+    content.push(headerRow());
+
+    for(var i = 0; i < rows; i++) {
+        var jan1st = birthDate.clone();
+        jan1st.setMonth(0);
+        jan1st.setDate(1);
+        jan1st.addYears(i);
+
+        var months = _.map(
+            _.range(12),
+            function(m) { var tmp = jan1st.clone(); tmp.setMonth(m); return tmp; }
+        );
+
+        content.push( dateRow({
+            yearCnt: i,
+            year: jan1st.getFullYear(),
+            months: months,
+            today: today
+        }));
+    }
+
+    var lg = document.getElementsByClassName('life-graph')[0];
+    lg.innerHTML = content.join('\n');
+}
+
+function main() {
+    var birthDate = createRoutes();
+    if(birthDate) {
+        drawLifeGraph(birthDate);
+    }
+}
+
+domready(function () {
+    main();  
+});
